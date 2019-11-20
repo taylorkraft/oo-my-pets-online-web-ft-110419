132316fb65ba55a95e904be9dcdff14dcbf1a24b
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-20 17:24:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  def intialize(name, owner)
+    @name = name
+    @owner = owner
+    mood = "nervous"
+  end
 end
