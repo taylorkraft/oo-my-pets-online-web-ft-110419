44e86e55259b5954a33127847a0eb92fd2af36f4
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-20 17:38:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,19 @@
 class Owner
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @species = "human"
+    @@all << self
   end 
   
   def say_species
     puts "I am a #{species}."
   end
   
+  def self.all 
+    @@all
+  end
 end
